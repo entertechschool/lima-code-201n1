5ea14eaa999c01899d25a816cf65fedeb3fe2ef6
bdiazc90 <bdiazc90@gmail.com>
2024-04-29 22:31:45 -0500

    Ideas sobre POO

--- class-07/demo/todoapp/js/app.js
+++ class-07/demo/todoapp/js/app.js
@@ -9,11 +9,44 @@
 function Task(text, dueDate) {
   this.text = text;
   this.dueDate = dueDate;
+  // Si la funcion "consola" ocupara 15Kb en Ram, después de instanciar 20 objetos del tipo Task, habrás ocupado: 300Kb.
+  this.consola = function () {
+    console.log(this.text + " - " + this.dueDate);
+  };
 }
 
-// const tarea1 = new Task("Tarea 1", "29/Abril");
-// const tarea2 = new Task("Tarea 2", "30/Abril");
-// const tarea3 = new Task("Tarea 3", "01/Mayo");
+// Si la funcion "imprimir" ocupara 15Kb en Ram, después de instanciar 20 objetos del tipo Task, habrás ocupado: 15Kb.
+Task.prototype.imprimir = function () {
+  console.log("Imprimiendo tarea: " + this.text);
+};
+
+const tarea1 = new Task("Tarea 1", "29/Abril"); // instancia del método consola (numero 1)
+const tarea2 = new Task("Tarea 2", "30/Abril"); // instancia del método consola (numero 2)
+const tarea3 = new Task("Tarea 3", "01/Mayo"); // instancia del método consola (numero 3)
+
+// Ejemplo de herencia por prototipo:
+
+function Animal() {
+  this.come = true;
+}
+const animal = {
+  come: true,
+};
+
+const conejo = {
+  salta: true,
+};
+
+const bugsbunny = {
+  gracioso: true,
+};
+
+conejo.__proto__ = new Animal();
+
+bugsbunny.__proto__ = conejo;
+
+console.log("Conejo: ", conejo);
+console.log("BugsBunny: ", bugsbunny);
 
 // console.log(tarea1);
 
@@ -50,12 +83,15 @@ addInput.addEventListener("keydown", function (eventoTeclaPresionada) {
   }
 });
 
+// Funciones Standalone:
 function addTask() {
   if (addInput.value == "" || addInput.value == undefined) {
     return;
   }
   console.log("Agregando tarea nueva: " + addInput.value);
   const task = new Task(addInput.value, "30/Abril");
+  task.consola();
+  task.imprimir();
 
   addInput.value = "";
 
--- class-07/lecture.md
+++ class-07/lecture.md
@@ -1,3 +1,7 @@
 # POO
 
-# Objetos de clase vs Objetos literales
+- Los objetos de clase permiten instanciar muchos objetos con la misma estructa de la clase modelo.
+- Para Javascript, todas los tipos de datos son finalmente Objetos y derivan de un solo modelo llamado `Object()`.
+- La forma de acceder a propiedades y métodos de mi clase 'padre' (superclase) es a través de mi prototipo.
+- El objeto raíz `Object` tiene pocas propiedades, y a medida que se van creando objetos usando la herencia, los nietos y bisnietos acumularán todas las propiedades heredadas de sus ancestros (prototipos).
+
